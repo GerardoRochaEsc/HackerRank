156dd5ab314142332ef3d5364d0c9777b2b6c523
Gerardo Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2023-10-31 01:20:36 -0600

    Update Mock_Test.js

--- Interview_Preparation_Kits/1_Week_Preparation_Kit/Mock_Test.js
+++ Interview_Preparation_Kits/1_Week_Preparation_Kit/Mock_Test.js
@@ -1,3 +1,5 @@
+//Find the median number of an odd array of integers and return the position where it is.
+
 'use strict';
 
 const fs = require('fs');
@@ -34,8 +36,11 @@ function readLine() {
 function findMedian(arr) {
     // Write your code here
     let index=0;
+    //sort() method needs an arrow function like that to sort the integers in ascending order
     arr.sort((a,b) => a-b);
+    //using Math.floor to round down the division of the size of the array / 2
     index=Math.floor(arr.length / 2)
+    //returning the Median number index
     return arr[index];
 }
 
