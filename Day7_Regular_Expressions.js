5143817a9adbc473d60923c9d055530caeb465d9
Gerardo Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2023-11-15 21:29:38 -0600

    Delete Day7_Regular_Expressions.js

--- Day7_Regular_Expressions.js
+++
@@ -1,8 +0,0 @@
-//Complete the function in the editor below by returning a RegExp object, , 
-//that matches any string  that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u.
-
-function regexVar() {
-    let re;
-    re = /^([aeiou]).*\1$/i;
-    return re;
-    };
